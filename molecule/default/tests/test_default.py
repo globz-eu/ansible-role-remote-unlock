250e7316f5f452f17a4dc64d9f9741e1b089f4c0
Stefan Dieterle <stefand@artcom.de>
2020-01-23 21:39:47 +0100

    Adapt tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,23 +8,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_dropbear_initramfs_package(host):
-    dropbear = host.package('dropbear-initramfs')
+    packages = ['dropbear', 'initramfs-tools', 'busybox']
 
-    assert dropbear.is_installed
+    for package in packages:
+        pkg = host.package(package)
+
+        assert pkg.is_installed
 
 
 def test_dropbear_config(host):
-    config = host.file('/etc/dropbear-initramfs/config')
+    config = host.file('/etc/default/dropbear')
 
     assert config.exists
     assert config.user == 'root'
     assert config.group == 'root'
     assert oct(config.mode) == '0o644'
-    assert config.contains(
-        r'^DROPBEAR_OPTIONS="'
-        r'-R -E -s -j -k -K 20 -I 120 -p 10022 -c /usr/bin/cryptroot-unlock"$'
-    )
-    assert config.contains(r'^IFDOWN=\*$')
+    assert config.contains(r'^NO_START=1$')
 
 
 def test_dropbear_authorized_keys(host):
@@ -37,16 +36,37 @@ def test_dropbear_authorized_keys(host):
     assert authorized_keys.contains(r'ssh-rsa AAAABLABLA== user@host')
 
 
-def test_default_grub(host):
-    default_grub = host.file('/etc/default/grub')
+def test_unlock_script(host):
+    crypt_unlock = host.file('/etc/initramfs-tools/hooks/crypt_unlock.sh')
 
-    assert default_grub.exists
-    assert default_grub.user == 'root'
-    assert default_grub.group == 'root'
-    assert oct(default_grub.mode) == '0o644'
-    assert default_grub.contains(
-        r'^GRUB_CMDLINE_LINUX="'
-        r'net\.ifnames=0 '
-        r'biosdevname=0 '
-        r'ip=1\.2\.3\.4::1\.2\.3\.1:255\.255\.255\.0::eth0:off"$'
+    assert crypt_unlock.exists
+    assert crypt_unlock.user == 'root'
+    assert crypt_unlock.group == 'root'
+    assert oct(crypt_unlock.mode) == '0o750'
+    assert (
+        crypt_unlock.sha256sum ==
+        'f831eaa22cdce1acc742efcc4c5571b86ae967194d24b23f15609762162ca2e4'
     )
+
+
+def test_cleanup_script(host):
+    cleanup = host.file('/etc/initramfs-tools/scripts/init-bottom/cleanup.sh')
+
+    assert cleanup.exists
+    assert cleanup.user == 'root'
+    assert cleanup.group == 'root'
+    assert oct(cleanup.mode) == '0o750'
+    assert (
+        cleanup.sha256sum ==
+        'e778d8aefb988e5645488e20759fbb7326783ff89840706aa0e8fc00447ee328'
+    )
+
+
+def test_initramfs_conf(host):
+    initramfs_conf = host.file('/etc/initramfs-tools/initramfs.conf')
+
+    assert initramfs_conf.exists
+    assert initramfs_conf.user == 'root'
+    assert initramfs_conf.group == 'root'
+    assert oct(initramfs_conf.mode) == '0o644'
+    assert initramfs_conf.contains(r'^IP=:::::eth0:dhcp')
